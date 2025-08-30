efc71eebc49f0f58b7b2aa3224967ad66ec4eb8e
edu <eduardogm81@gmail.com>
2025-08-30 12:45:05 +0200

    Definir pipes globales

--- 02-car-dealership/src/cars/cars.controller.ts
+++ 02-car-dealership/src/cars/cars.controller.ts
@@ -4,7 +4,6 @@ import { CarsService } from './cars.service';
 import { CreateCarDto } from './dto/create-car.dto';
 
 @Controller('cars')
-@UsePipes(ValidationPipe)
 export class CarsController {
 
   constructor(
--- 02-car-dealership/src/main.ts
+++ 02-car-dealership/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true, // Sólo deja pasar las propiedades que estén definidas en el objeto
+        forbidNonWhitelisted: true, // Si envía más propiedades que no están definidas da un error
+      })
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
