563703d2702f877fd2d56cecf35371fad635a394
edu <eduardogm81@gmail.com>
2025-08-30 11:23:44 +0200

    Sección 3 - Primeros pasos en Nest, hasta lección 47 - DTO - Data transfer Object

---
+++ 02-car-dealership/src/cars/cars.controller.ts
@@ -0,0 +1,40 @@
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, ParseUUIDPipe, Patch, Post } from '@nestjs/common';
+import * as console from 'node:console';
+import { CarsService } from './cars.service';
+import { CreateCarDto } from './dto/create-car.dto';
+
+@Controller('cars')
+export class CarsController {
+
+
+  constructor(
+    private readonly carsService: CarsService
+  ) {}
+
+  @Get()
+  getAllCars() {
+    return this.carsService.findAll();
+  }
+
+  @Get(':id')
+  getCarById( @Param('id', ParseUUIDPipe) id: string ) {
+    console.log({ id })
+    return this.carsService.findOneById(id);
+  }
+
+  @Post()
+  createCar( @Body() createCarDto: CreateCarDto ) {
+    return { ok: true, method: 'POST', createCarDto };
+  }
+
+  @Patch(':id')
+  updateCar( @Param('id') id: string, @Body() body: any ) {
+    return { ok: true, method: 'Patch', id, body };
+  }
+
+  @Delete(':id')
+  deleteCar( @Param('id') id: string ) {
+    return { ok: true, method: 'Delete  ', id };
+  }
+
+}
