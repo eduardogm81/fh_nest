5ecec78460187f2040990caa7da051d795f312d4
e.gomez <e.gomez@garsa.com>
2025-09-01 09:24:00 +0200

    Control de errores

--- 03-pokedex/src/pokemon/pokemon.service.ts
+++ 03-pokedex/src/pokemon/pokemon.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 import { Model } from 'mongoose';
@@ -15,8 +15,16 @@ export class PokemonService {
 
     async create(createPokemonDto: CreatePokemonDto) {
         createPokemonDto.name = createPokemonDto.name.toLowerCase();
-        const pokemon = await this.pokemonModel.create(createPokemonDto);
-        return pokemon;
+        try {
+            const pokemon = await this.pokemonModel.create(createPokemonDto);
+            return pokemon;
+        } catch (error) {
+            if (error.code === 11000) {
+                throw new BadRequestException(`Pokemon already exists ${ JSON.stringify(error.keyValue) }`);
+            }
+            console.log(error);
+            throw new InternalServerErrorException(`Can't create Pokemon - Check server logs`);
+        }
     }
 
     findAll() {
