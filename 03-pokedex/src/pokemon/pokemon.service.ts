f9e98dc8d7f8359a28764d682d7443bacfcf260a
e.gomez <e.gomez@garsa.com>
2025-09-01 09:55:56 +0200

    Buscar por un valor y actualizar

--- 03-pokedex/src/pokemon/pokemon.service.ts
+++ 03-pokedex/src/pokemon/pokemon.service.ts
@@ -1,7 +1,12 @@
-import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import {
+    BadRequestException,
+    Injectable,
+    InternalServerErrorException,
+    NotFoundException,
+} from '@nestjs/common';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
-import { Model } from 'mongoose';
+import { isValidObjectId, Model } from 'mongoose';
 import { Pokemon } from './entities/pokemon.entity';
 import { InjectModel } from '@nestjs/mongoose';
 
@@ -31,12 +36,35 @@ export class PokemonService {
         return `This action returns all pokemon`;
     }
 
-    findOne(id: number) {
-        return `This action returns a #${id} pokemon`;
+    async findOne(term: string) {
+        let pokemon: Pokemon | null = null;
+
+        if (!isNaN(+term)) { // Si es un número
+            pokemon = await this.pokemonModel.findOne({'no': term});
+        }
+
+        // MongoID
+        if (!pokemon && isValidObjectId(term)) {
+            pokemon = await this.pokemonModel.findById(term);
+        }
+
+        // Name
+        if (!pokemon) {
+            pokemon = await this.pokemonModel.findOne({'name': term.toLowerCase().trim()})
+        }
+
+        if (!pokemon) {
+            throw new NotFoundException(`Pokemon with id/name/no ${term} Not found`);
+        }
+
+        return pokemon;
     }
 
-    update(id: number, updatePokemonDto: UpdatePokemonDto) {
-        return `This action updates a #${id} pokemon`;
+    async update(term: string, updatePokemonDto: UpdatePokemonDto) {
+        const pokemon = await this.findOne(term);
+        if (updatePokemonDto.name) updatePokemonDto.name = updatePokemonDto.name.toLowerCase().trim();
+        await pokemon.updateOne(updatePokemonDto);
+        return { ...pokemon.toJSON(), ...updatePokemonDto };
     }
 
     remove(id: number) {
