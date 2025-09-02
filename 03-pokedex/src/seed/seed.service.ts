37e8c71e9042580f3c2a8fc762bb743ddbc2d2ec
e.gomez <e.gomez@garsa.com>
2025-09-02 10:33:41 +0200

    Sacar axios a un adaptador

--- 03-pokedex/src/seed/seed.service.ts
+++ 03-pokedex/src/seed/seed.service.ts
@@ -3,20 +3,22 @@ import { PokeResponse } from './interfaces/poke-response.interface';
 import { InjectModel } from '@nestjs/mongoose';
 import { Pokemon } from '../pokemon/entities/pokemon.entity';
 import { Model } from 'mongoose';
+import { AxiosAdapter } from '../common/adapters/axios.adapter';
 
 @Injectable()
 export class SeedService {
 
 
     constructor(
-        @InjectModel( Pokemon.name ) private readonly pokemonModel: Model<Pokemon>
+        @InjectModel( Pokemon.name ) private readonly pokemonModel: Model<Pokemon>,
+        private readonly http: AxiosAdapter
     ) {
     }
 
     async executeSeed() {
         await this.pokemonModel.deleteMany();
 
-        const {data} = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650')
+        const data: PokeResponse = await this.http.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650')
 
         const pokemonToInsert: { name: string, no: number }[] = [];
 
